b41c838ec8cf8ced3c8d5e71dea7c3964baf8351
Bobby Hall <bobbyhall@Bobbys-MacBook-Pro.local>
2024-02-10 16:12:35 -0500

    stuff

--- src/app/code-snippets/signIn/page.tsx
+++ src/app/code-snippets/signIn/page.tsx
@@ -4,7 +4,7 @@ import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import { loginWithEmail, signUpWithGitHub } from '../../../lib/supabaseUtils/auth'
+import { loginWithEmail, signUpWithGitHub } from '@/lib/supabaseUtils/auth'
 import { useRouter } from 'next/navigation'
 
 export default function Component() {
